2179a07173304aeefad22dbbdf18ea1e8bfe236c
long.yu <yulong5201@gmail.com>
2023-09-20 21:21:30 +0800

    Update ICachePageProvider.h

--- OrthancFramework/Sources/Cache/ICachePageProvider.h
+++ OrthancFramework/Sources/Cache/ICachePageProvider.h
@@ -5,5 +5,14 @@
 
 namespace Orthanc
 {
-    
+    namespace Deprecated
+    {
+        class ICachePageProvider
+        {
+        public:
+            virtual ~ICachePageProvider() {}
+
+            virtual IDynamicObject* Provide(const std::string& id) = 0;
+        }
+    }
 }
