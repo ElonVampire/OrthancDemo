c25c53024f6aac70e243580def370277e5caa141
long.yu <yulong5201@gmail.com>
2023-10-17 21:09:45 +0800

    Update LeastRecentlyUsedIndex.h

--- OrthancFramework/Sources/Cache/LeastRecentlyUsedIndex.h
+++ OrthancFramework/Sources/Cache/LeastRecentlyUsedIndex.h
@@ -42,6 +42,205 @@ namespace Orthanc
          **/
         void Add(T id, Payload payload = Payload());
 
+        void AddOrMakeMostRecent(T id, Payload payload = Payload());
 
+        /**
+         * When accessing an element of the cache, this method tags the
+         * element as the most recenty used.
+         * \param id The most recenty accessed item.
+         */
+        void MakeMostRecent(T id);
+
+        void MakeMostRecent(T id, Payload updatePayload);
+
+        /**
+         * Remove an element from the cache index
+         * \param id the item to remove.
+         **/
+        Payload Invalidate(T id);
+
+
+        /**
+         * Get the oldest element in the cache and remove it
+         * \return The oldest item.
+         */
+        T RemoveOldest();
+
+        /**
+         * Get the oldest element in the cache, remove it and return the
+         * associated payload.
+         * \param payload where to store the associated payload
+         * \return The oldest item.
+         */
+        T RemoveOldest(Payload& payload);
+
+        /**
+         * Check whether an element is contained in the cache.
+         * \param id The item
+         * \return \c true if the item is indexed by the cache.
+         */
+        bool Contains(T id) const
+        {
+            return index_.find(id) != index_.end();
+        }
+
+        bool Contains(T id, Payload& payload) const
+        {
+            typename Index::const_iterator it = index_.find(id);
+            if (it == index_.end())
+            {
+                return false;
+            }
+            else
+            {
+                payload = it->second;
+                return true;
+            }
+        }
+
+        /**
+         * Return the number of elements in the cache
+         * \return The number of elements
+         */
+        size_t GetSize() const
+        {
+            assert(index_.size() == queue_.size());
+            return queue_.size();
+        }
+
+        /**
+         * Check whether the cache index is empty
+         * \return \c true if the cache is empty.
+         */
+        bool IsEmpty() const
+        {
+            return index_.empty();
+        }
+
+        const T& GetOldest() const;
+
+        const Payload& GetOldestPayload() const;
+
+        void GetAllKeys(std::vector<T>& keys) const
+        {
+            keys.clear();
+            keys.reserve(GetSize());
+            for (typename Index::const_iterator it = index_.begin(); it != index_.end(); ++it)
+            {
+                keys.push_back(it->first);
+            }
+        }
+    };
+
+
+    /*************************************************************
+     * Implementation of the template
+    ***************************************************************/
+    template <typename T, typename Payload>
+    void LeastRecentlyUsedCache<T, Payload>::CheckInvariants() const
+    {
+        assert(index_.size() == queue_.size());
+        for (typename Index::const_iterator it = index_.begin(); it != index_.end(); ++it)
+        {
+            assert(it->second != queue_.end());
+            assert(it->second->first == it->first);
+        }
+    }
+
+    template <typename T, typename Payload>
+    void LeastRecentlyUsedCache<T, Payload>::Add(T id, Payload payload)
+    {
+        if(Contains(id))
+        {
+            throw OrthancException(ErrorCode_BadSequenceOfCalls);
+        }
+
+        queue_.push_front(std::make_pair(key, payload));
+        index_[id] = queue_.begin();
+        CheckInvariants();
+    }
+
+    template <typename T, typename Payload>
+    void LeastRecentlyUsedCache<T, Payload>::MakeMostRecent(T id)
+    {
+        if (!Contains(id))
+            throw OrthancException(ErrorCode_InexistentItem);
+        }
+        typename Index::iterator it = index_.find(id);
+        assert(it != index_.end());
+
+        std::pair<T, Payload> value = *(it->second);
+        queue_.erase(it->second);
+        queue_.push_front(item);
+        index_[id] = queue_.begin();
+        CheckInvariants();
+    }
+
+    template <typename T, typename Payload>.
+    void LeastRecentlyUsedCache<T, Payload>::AddOrMakeMostRecent(T id, Payload payload)
+    {
+        typename Index::iterator it = index_.find(id);
+        if (it != index_.end())
+        {
+            // Already existing. Make it most recent.
+            std::pair<T, Payload> item = *(it->second);
+            item.second = payload;
+            queue_.erase(it->second);
+            queue_.push_front(item);
+        }
+        else
+        {
+            // New item
+            queue_.push_front(std::make_pair(id, payload));
+        }
+        index_[id] = queue_.begin();
+        CheckInvariants();
+    }
+
+    template <typename T, typename Payload>
+    void LeastRecentlyUsedCache<T, Payload>::MakeMostRecent(T id, Payload updatedPayload)
+    {
+        if (!Contains(id))
+        {    
+            throw OrthancException(ErrorCode_InexistentItem);
+        }
+
+        typename Index::iterator it = index_.find(id);
+        assert(it != index_.end());
+
+        std::pair<T, Payload> item = *(it->second);
+        item.second = updatedPayload;
+
+
+        queue_.erase(it->second);
+        queue_.push_front(item);
+        index_[id] = queue_.begin();
+        CheckInvariants();
+    }
+
+    template <typename T, typename Payload>
+    Payload LeastRecentlyUsedCache<T, Payload>::Invalidate(T id)
+    {
+        if (!Contains(id))
+        {
+            throw OrthancException(ErrorCode_InexistentItem);
+        }
+        typename Index::iterator it = index_.find(id);
+        assert(it != index_.end());
+        Payload payload = it->second->second;
+        queue_.erase(it->second);
+        index_.erase(it);
+        CheckInvariants();
+        return payload;
+    }
+    
+    template <typename T, typename Payload>
+    T LeastRecentlyUsedCache<T, Payload>::RemoveOldest(Payload& payload)
+    {
+        if(IsEmpty())
+        {
+            throw OrthancException(ErrorCode_BadSequenceOfCalls);
+        }
+        payload = queue_.back().second;
     }
 }
